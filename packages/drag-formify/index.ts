65fedfadc0aabe76cb0c3cdd9b9c95a11789daa5
parminder-futurionic <parminder.futurionic@gmail.com>
2023-07-25 00:07:43 +0530

    first commit

---
+++ packages/drag-formify/index.ts
@@ -0,0 +1,3 @@
+import Button from "./src/components";
+
+export { Button };
---
+++ packages/drag-formify/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Button from './components'
+
+function App() {
+
+  return (
+    <>
+     <Button />
+    </>
+  )
+}
+
+export default App
